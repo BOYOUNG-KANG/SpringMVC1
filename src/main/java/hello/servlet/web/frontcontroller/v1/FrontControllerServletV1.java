35e1e2559f1008708df7ee02432fb1c1cf1353ff
BOYOUNG-KANG <28287@naver.com>
2023-07-01 16:42:48 +0900

    [feat] view 분리v2

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -1,5 +1,6 @@
 package hello.servlet.web.frontcontroller.v1;
 
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
@@ -13,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV", urlPatterns = "/front-controller/v1/*")
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
